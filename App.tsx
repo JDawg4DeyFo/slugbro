256acb85892ab9e7373b91720ee90a123fa09559
Jacob <jdennon@ucsc.edu>
2024-03-02 20:58:13 -0800

    Starting work on home page prototype

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Main React imports
+import {Text, View } from 'react-native';
+
+// Slugbro component imports
+import Styles from './src/Styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={Styles.RootContainer}>
+      <View style={Styles.HomeHeader}>
+        <Text>Hello</Text>
+        <Text>Hello2</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/Styles.tsx
+++ src/Styles.tsx
@@ -1,29 +1,60 @@
 import {StyleSheet} from 'react-native';
 // NOTE: Rem units assume 16px root font size.
 
-
 // Color Variables
-const DarkestGrey = '#21252A';
+// Higher number = darker
+
+
+export const Colors = {
+	// Neutrals
+	Black			: '#000000',
+	DarkestGrey		: '#21252A',
+	Grey12        	: '#343A40',
+	Grey10        	: '#495057',
+	Grey8         	: '#868E96',
+	Grey6         	: '#ADB5BD',
+	Grey4         	: '#CFD4DA',
+	Grey3        	: '#E0E3E7',
+	Grey1         	: '#F1F3F5',
+	White			: '#FFFFFF',
+
+	// Primaries
+
+	// Derivatives
+
+	// Reds
+
+	// Greens
+
+	// Yellows
+};
+
+export const TextSize  = {
+	Rem3	: 48,
+	Rem2	: 32,
+
+	Regular	: 15,
+	Small	: 11,
+}
 
 
 const Styles = StyleSheet.create({
-    // CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER CONTAINER
-    // Container backgrounds: Profile Icon, bro container in feed
-    MainContainer: {
-        flex: 100,
-    },
-
-
-    // TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT
-    // 3Rem text
-    Size3Text: {
-        fontSize: 48    // NOTE: I would prefer to use REM, but that's not supported.
-    },
-
-    // For main text of container
-    DarkText: {
-        color: DarkestGrey,
-    },
+	// One container to rule them all...
+	RootContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		backgroundColor: Colors.Grey1,
+		paddingHorizontal: 20,
+		paddingTop: 12,
+	},
+	
+	// Home page header, holds settings and profile icon
+	HomeHeader: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent:'flex-end',
+		alignItems: 'center',
+	},
 
 });
 
