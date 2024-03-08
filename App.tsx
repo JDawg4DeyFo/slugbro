be32fe536098d47c9af47d731c24fc8820dcf9ae
Jacob <jacob.dennon@gmail.com>
2024-03-07 21:09:15 -0800

    Added font loading

--- App.tsx
+++ App.tsx
@@ -2,10 +2,21 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import * as NavigationBar from "expo-navigation-bar";
+import * as Font from 'expo-font';
 // Slugbro component imports
 import StackNavigator from './src/Stack';
 
+const loadFonts = async () => {
+  await Font.loadAsync({
+    'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+    'Inter-Bold': require('Inter-Bold.ttf')
+  });
+};
+
 export default function App() {
+  // Load fonts
+  loadFonts();
+
   // Hide navigation bar
   // I don't really know what this code does
   const [barVisibility, setBarVisibility] = useState();
