d8568f1d33c79a7c8c9a9738bd4a9702fee992d7
Jacob <jacob.dennon@gmail.com>
2024-03-13 17:51:14 -0700

    Started work on BroPage

---
+++ src/BroConnections.tsx
@@ -0,0 +1,193 @@
+import React from "react";
+import { FlatList, View, Text, Image, ImageSourcePropType } from 'react-native';
+
+import StylesObj from './Styles'
+const Styles = StylesObj.StylesObj;
+
+// Data before server stuff is finished
+const PREDEBUG = [
+    {
+        id: 1,
+        Name: 'JDawg',
+        TotalBros: 38103,
+        Pfp: require('../assets/SamplePFP.jpg')
+    },
+    {
+        id: 1,
+        Name: 'LolFactor',
+        TotalBros: 1231,
+        Pfp: require('../assets/home.png')
+    },
+    {
+        id: 1,
+        Name: 'jaaaa',
+        TotalBros: 517,
+        Pfp: require('../assets/splash.png')
+    },
+    {
+        id: 1,
+        Name: 'Poopface',
+        TotalBros: 420,
+        Pfp: require('../assets/adaptive-icon.png')
+    },
+    {
+        id: 1,
+        Name: 'JDawg',
+        TotalBros: 38103,
+        Pfp: require('../assets/SamplePFP.jpg')
+    },
+    {
+        id: 1,
+        Name: 'LolFactor',
+        TotalBros: 1231,
+        Pfp: require('../assets/home.png')
+    },
+    {
+        id: 1,
+        Name: 'jaaaa',
+        TotalBros: 517,
+        Pfp: require('../assets/splash.png')
+    },
+    {
+        id: 1,
+        Name: 'Poopface',
+        TotalBros: 420,
+        Pfp: require('../assets/adaptive-icon.png')
+    },
+    {
+        id: 1,
+        Name: 'JDawg',
+        TotalBros: 38103,
+        Pfp: require('../assets/SamplePFP.jpg')
+    },
+    {
+        id: 1,
+        Name: 'LolFactor',
+        TotalBros: 1231,
+        Pfp: require('../assets/home.png')
+    },
+    {
+        id: 1,
+        Name: 'jaaaa',
+        TotalBros: 517,
+        Pfp: require('../assets/splash.png')
+    },
+    {
+        id: 1,
+        Name: 'Poopface',
+        TotalBros: 420,
+        Pfp: require('../assets/adaptive-icon.png')
+    },
+    {
+        id: 1,
+        Name: 'JDawg',
+        TotalBros: 38103,
+        Pfp: require('../assets/SamplePFP.jpg')
+    },
+    {
+        id: 1,
+        Name: 'LolFactor',
+        TotalBros: 1231,
+        Pfp: require('../assets/home.png')
+    },
+    {
+        id: 1,
+        Name: 'jaaaa',
+        TotalBros: 517,
+        Pfp: require('../assets/splash.png')
+    },
+    {
+        id: 1,
+        Name: 'Poopface',
+        TotalBros: 420,
+        Pfp: require('../assets/adaptive-icon.png')
+    },
+    {
+        id: 1,
+        Name: 'JDawg',
+        TotalBros: 38103,
+        Pfp: require('../assets/SamplePFP.jpg')
+    },
+    {
+        id: 1,
+        Name: 'LolFactor',
+        TotalBros: 1231,
+        Pfp: require('../assets/home.png')
+    },
+    {
+        id: 1,
+        Name: 'jaaaa',
+        TotalBros: 517,
+        Pfp: require('../assets/splash.png')
+    },
+    {
+        id: 1,
+        Name: 'Poopface',
+        TotalBros: 420,
+        Pfp: require('../assets/adaptive-icon.png')
+    },
+    {
+        id: 1,
+        Name: 'JDawg',
+        TotalBros: 38103,
+        Pfp: require('../assets/SamplePFP.jpg')
+    },
+    {
+        id: 1,
+        Name: 'LolFactor',
+        TotalBros: 1231,
+        Pfp: require('../assets/home.png')
+    },
+    {
+        id: 1,
+        Name: 'jaaaa',
+        TotalBros: 517,
+        Pfp: require('../assets/splash.png')
+    },
+    {
+        id: 1,
+        Name: 'Poopface',
+        TotalBros: 420,
+        Pfp: require('../assets/adaptive-icon.png')
+    },
+
+]
+const DEBUG_DATA = PREDEBUG.map((item, index) => ({
+    ...item,
+    id: index.toString(),
+}));
+const DATA = DEBUG_DATA;
+
+
+// Props of flatlist items
+type EntryItemProps = {
+    Name: string,
+    TotalBros: number,
+    Pfp: ImageSourcePropType,
+};
+
+// Object template to populate flatlist
+const EntryItem = ({ Name, TotalBros, Pfp }: EntryItemProps) => {
+    return (
+        <View style={Styles.LBE_Container}>
+            <View style={Styles.LBE_NamePFP}>
+                <Image style={Styles.LBE_PFP} source={Pfp}/>
+                <Text style={Styles.LBE_Name}>{Name}</Text>
+            </View>
+            <Text style={Styles.LBE_BrosSent}>{TotalBros} Bros</Text>
+        </View>
+    );
+};
+
+const BroConnections = () => {
+    return (
+        <FlatList 
+            data={DATA}
+            renderItem={({item}) => <EntryItem Name={item.Name} TotalBros={item.TotalBros} Pfp={item.Pfp}/>}
+            keyExtractor={item => item.id}
+            showsVerticalScrollIndicator ={false}
+        />
+    );
+};
+
+export default BroConnections;
--- src/Bros.tsx
+++ src/Bros.tsx
@@ -1,10 +1,48 @@
-import {View, Text} from 'react-native';
+import React from 'react';
+import { TouchableOpacity, View, Text, Image } from 'react-native';
 
-const Bros = () => {
+import StylesObj from './Styles';
+const Styles = StylesObj.StylesObj;
+
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from './Stack';
+import LBEntries from './LBEtnries';
+
+const Bros = ({navigation}: {navigation:StackNavigationProp<RootStackParamList>}) => {
     return(
-        <View>
-            <Text>Hello</Text>
+        <View style={Styles.RootContainer}>
+      <View style={Styles.FeedHeader}>
+        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+        <View style={Styles.ProfileContainer}>
+          <Image 
+          source={require('../assets/SamplePFP.jpg')}
+          style={Styles.ProfileIcon}
+          />
+          <View style={Styles.ProfileNameSloganContainer}>
+            <Text style={Styles.ProfileName}>JDawg</Text>
+            <Text style={Styles.ProfileSlogan}>"Born2Bro"</Text>
+          </View>
         </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+          <Image
+            source={require('../assets/settings.png')}
+            style={Styles.SettingsIcon}
+          />
+        </TouchableOpacity>
+      </View>
+
+        <View style={Styles.LBContainer}>
+            <Text style={Styles.LBTitle}>Bros</Text>
+            <View style={Styles.LBHeader}>
+                <Text style={Styles.LBHeadText}>Your Bros</Text>
+                <Text style={Styles.LBHeadText}>XX Total Bros</Text>
+            </View>
+            <LBEntries/>
+        </View>
+      
+    </View>
 )};
 
 export default Bros;
