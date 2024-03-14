2efd0f4979ab933814c91bd311b84fbe2e4a71b7
Jacob <jacob.dennon@gmail.com>
2024-03-13 17:55:25 -0700

    Modifying base file for bro page

--- src/BroConnections.tsx
+++ src/BroConnections.tsx
@@ -9,145 +9,121 @@ const PREDEBUG = [
     {
         id: 1,
         Name: 'JDawg',
-        TotalBros: 38103,
         Pfp: require('../assets/SamplePFP.jpg')
     },
     {
         id: 1,
         Name: 'LolFactor',
-        TotalBros: 1231,
         Pfp: require('../assets/home.png')
     },
     {
         id: 1,
         Name: 'jaaaa',
-        TotalBros: 517,
         Pfp: require('../assets/splash.png')
     },
     {
         id: 1,
         Name: 'Poopface',
-        TotalBros: 420,
         Pfp: require('../assets/adaptive-icon.png')
     },
     {
         id: 1,
         Name: 'JDawg',
-        TotalBros: 38103,
         Pfp: require('../assets/SamplePFP.jpg')
     },
     {
         id: 1,
         Name: 'LolFactor',
-        TotalBros: 1231,
         Pfp: require('../assets/home.png')
     },
     {
         id: 1,
         Name: 'jaaaa',
-        TotalBros: 517,
         Pfp: require('../assets/splash.png')
     },
     {
         id: 1,
         Name: 'Poopface',
-        TotalBros: 420,
         Pfp: require('../assets/adaptive-icon.png')
     },
     {
         id: 1,
         Name: 'JDawg',
-        TotalBros: 38103,
         Pfp: require('../assets/SamplePFP.jpg')
     },
     {
         id: 1,
         Name: 'LolFactor',
-        TotalBros: 1231,
         Pfp: require('../assets/home.png')
     },
     {
         id: 1,
         Name: 'jaaaa',
-        TotalBros: 517,
         Pfp: require('../assets/splash.png')
     },
     {
         id: 1,
         Name: 'Poopface',
-        TotalBros: 420,
         Pfp: require('../assets/adaptive-icon.png')
     },
     {
         id: 1,
         Name: 'JDawg',
-        TotalBros: 38103,
         Pfp: require('../assets/SamplePFP.jpg')
     },
     {
         id: 1,
         Name: 'LolFactor',
-        TotalBros: 1231,
         Pfp: require('../assets/home.png')
     },
     {
         id: 1,
         Name: 'jaaaa',
-        TotalBros: 517,
         Pfp: require('../assets/splash.png')
     },
     {
         id: 1,
         Name: 'Poopface',
-        TotalBros: 420,
         Pfp: require('../assets/adaptive-icon.png')
     },
     {
         id: 1,
         Name: 'JDawg',
-        TotalBros: 38103,
         Pfp: require('../assets/SamplePFP.jpg')
     },
     {
         id: 1,
         Name: 'LolFactor',
-        TotalBros: 1231,
         Pfp: require('../assets/home.png')
     },
     {
         id: 1,
         Name: 'jaaaa',
-        TotalBros: 517,
         Pfp: require('../assets/splash.png')
     },
     {
         id: 1,
         Name: 'Poopface',
-        TotalBros: 420,
         Pfp: require('../assets/adaptive-icon.png')
     },
     {
         id: 1,
         Name: 'JDawg',
-        TotalBros: 38103,
         Pfp: require('../assets/SamplePFP.jpg')
     },
     {
         id: 1,
         Name: 'LolFactor',
-        TotalBros: 1231,
         Pfp: require('../assets/home.png')
     },
     {
         id: 1,
         Name: 'jaaaa',
-        TotalBros: 517,
         Pfp: require('../assets/splash.png')
     },
     {
         id: 1,
         Name: 'Poopface',
-        TotalBros: 420,
         Pfp: require('../assets/adaptive-icon.png')
     },
 
@@ -162,19 +138,17 @@ const DATA = DEBUG_DATA;
 // Props of flatlist items
 type EntryItemProps = {
     Name: string,
-    TotalBros: number,
     Pfp: ImageSourcePropType,
 };
 
 // Object template to populate flatlist
-const EntryItem = ({ Name, TotalBros, Pfp }: EntryItemProps) => {
+const EntryItem = ({ Name, Pfp }: EntryItemProps) => {
     return (
         <View style={Styles.LBE_Container}>
             <View style={Styles.LBE_NamePFP}>
                 <Image style={Styles.LBE_PFP} source={Pfp}/>
                 <Text style={Styles.LBE_Name}>{Name}</Text>
             </View>
-            <Text style={Styles.LBE_BrosSent}>{TotalBros} Bros</Text>
         </View>
     );
 };
@@ -183,7 +157,7 @@ const BroConnections = () => {
     return (
         <FlatList 
             data={DATA}
-            renderItem={({item}) => <EntryItem Name={item.Name} TotalBros={item.TotalBros} Pfp={item.Pfp}/>}
+            renderItem={({item}) => <EntryItem Name={item.Name} Pfp={item.Pfp}/>}
             keyExtractor={item => item.id}
             showsVerticalScrollIndicator ={false}
         />
