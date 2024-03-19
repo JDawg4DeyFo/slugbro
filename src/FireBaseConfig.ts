4ff2a3eb9c72ec02f931bf20b87871b7b11dbb45
Jacob <jacob.dennon@gmail.com>
2024-03-18 17:54:12 -0700

    need to do this right so I'll do it tommorow with more time

--- src/FireBaseConfig.ts
+++ src/FireBaseConfig.ts
@@ -17,3 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+
+
+// continue with
+// https://firebase.google.com/docs/auth/web/start
