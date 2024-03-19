e05cd7a2220a0407aacf91ec8b94324073f0c579
Jacob <jacob.dennon@gmail.com>
2024-03-18 17:52:39 -0700

    starting firebase integration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push('cjs');
+
+module.exports = defaultConfig;
---
+++ src/FireBaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAA-APiG3N5sRxzdSvLdrrs1Nu_0vwCuw",
+  authDomain: "slugbro-630b8.firebaseapp.com",
+  projectId: "slugbro-630b8",
+  storageBucket: "slugbro-630b8.appspot.com",
+  messagingSenderId: "126844262942",
+  appId: "1:126844262942:web:6b10561408c10328e6ec0f",
+  measurementId: "G-4WCJF25E49"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
