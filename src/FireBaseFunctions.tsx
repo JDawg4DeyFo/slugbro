23ed3e9b566418dc06019d556d9ab753db3feabf
Jacob <jacob.dennon@gmail.com>
2024-03-19 21:07:15 -0700

    started real work on firebase auth

--- src/FireBaseFunctions.tsx
+++ src/FireBaseFunctions.tsx
@@ -1,1 +1,16 @@
-// idk what i'm doing here lol
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { FIREBASE_APP, FIREBASE_AUTH } from "./FireBaseConfig";
+const Auth = FIREBASE_AUTH;
+
+
+// Function for user sign ups
+type SignUpProps = {
+    email: string,
+    password: string,
+};
+
+export const SignUp = async ({email, password}: SignUpProps) => {
+    try {
+        const response = await createUserWithEmailAndPassword(Auth, email, password);
+    }
+}
