eebed1909742c6e6f9344e9a45d1b5e4aff7c65a
Jacob <jacob.dennon@gmail.com>
2024-03-19 21:15:42 -0700

    Finished draft for sign in and sign up

--- src/FireBaseFunctions.tsx
+++ src/FireBaseFunctions.tsx
@@ -1,16 +1,35 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { FIREBASE_APP, FIREBASE_AUTH } from "./FireBaseConfig";
 const Auth = FIREBASE_AUTH;
 
 
 // Function for user sign ups
 type SignUpProps = {
-    email: string,
-    password: string,
+    Email: string,
+    Password: string,
 };
-
-export const SignUp = async ({email, password}: SignUpProps) => {
+export const SignUp = async ({Email, Password}: SignUpProps) => {
     try {
-        const response = await createUserWithEmailAndPassword(Auth, email, password);
+        const response = await createUserWithEmailAndPassword(Auth, Email, Password);
+        console.log(response);
+    } catch (error: any) {
+        console.log(error);
+        alert('Sign up failed: ' + error.message);
+    }
+}
+
+
+// Function for sign in
+type SignInProps = {
+    Email: string,
+    Password: string,
+};
+export const UserSignIn = async ({Email, Password}: SignInProps) => {
+    try {
+        const response = await signInWithEmailAndPassword(Auth, Email, Password);
+        console.log(response);
+    } catch (error: any) {
+        console.log(error);
+        alert('Sign in failed: ' + error.message);
     }
 }
