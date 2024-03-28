57cb6c34bf7809ad743413950cc97254f16bef50
Daniel Chang <dawichan@ucsc.edu>
2024-03-28 13:11:33 -0700

    updated babel config so it compiles

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.sourceExts.push('cjs');
-
-module.exports = defaultConfig;
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo']
+  };
+};
