b6934ae56e7c38b6dfa323e75e3200662a5148f1
Jacob Dennon <jdennon@ucsc.edu>
2024-03-30 11:59:58 -0700

    added info page (formerly settings)

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -9,15 +9,18 @@ const Settings = () => {
         <View style={Styles.InfoRootContainer}>
             <View style={Styles.InformationSection}>
                 <Text style={Styles.InformationHeader}>What is this?</Text>
-                <Text style={Styles.PH_InfotainerParagraph}>Testing, est thing since wrestling</Text>
+                <Text style={Styles.PH_InfotainerParagraph}>SlugBro is an April fool's app created by Jacob Dennon and Daniel Chang.</Text>
             </View>
             <View style={Styles.InformationSection}>
-                <Text style={Styles.InformationHeader}>What is this?</Text>
-                <Text style={Styles.PH_InfotainerParagraph}>Testing, est thing since wrestling</Text>
+                <Text style={Styles.InformationHeader}>About Jacob</Text>
+                <Text style={Styles.PH_InfotainerParagraph}>I'm a second year studying Electrical Engineering. I like soccer, basketball, running, biking, swimming, and reading. I come from Auburn, CA, but I have no idea where I'm gonna go after I graduate.</Text>
+                <Text style={Styles.PH_InfotainerParagraph}>Let me know if you wanna do some biking on the mountain gravel roads of San Benito County</Text>
+                <Text style={Styles.PH_InfotainerParagraph}>PSA: join clubs! They're good for you and they're good for the community. Imagine if 90% of undergrads were active participants in some club... that would be so cool.</Text>
+                <Text style={Styles.PH_InfotainerParagraph}></Text>
             </View>
             <View style={Styles.InformationSection}>
-                <Text style={Styles.InformationHeader}>What is this?</Text>
-                <Text style={Styles.PH_InfotainerParagraph}>Testing, est thing since wrestling</Text>
+                <Text style={Styles.InformationHeader}>About Daniel</Text>
+                <Text style={Styles.PH_InfotainerParagraph}>Daniel hasn't add his bio yet.</Text>
             </View>
         </View>
     );
