e9e5cc2ddde165e61f45ee8fa3c8a85804af6940
Jacob <jacob.dennon@gmail.com>
2024-04-02 10:04:46 -0700

    added info on login page for users hesitant to create an account

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
-import {Text, View, TouchableOpacity} from 'react-native'
+import { Text, View, TouchableOpacity, Image } from 'react-native'
 // Style
 import StylesObj from './Styles';
 import { TextInput } from 'react-native-gesture-handler';
 const Styles = StylesObj.StylesObj;
 // Firebase
 import { UserSignUp, UserSignIn } from './FireBaseFunctions';
+// Stack
+import { RootStackParamList } from './Stack';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 
-const Login = () => {
+const Login = ({navigation}: {navigation: StackNavigationProp<RootStackParamList>}) => {
     const [Email, ChangeEmail] = React.useState('');
     const [Password, ChangePassword] = React.useState('');
     const [loginDisabled, setLoginDisabled] = useState(false);
     const [signupDisabled, setSignupDisabled] = useState(false);
 
-    return(
+    return (
         <View style={Styles.RootContainer}>
             <View style={Styles.LoginHeader}>
                 <Text style={Styles.LoginHeaderText}>SlugBro</Text>
             </View>
-            
+
             <View style={Styles.MainLogin}>
                 <Text style={Styles.MainLoginTitle}>Login, Or Sign Up!</Text>
                 <View style={Styles.TextInputTitleRow}>
@@ -30,7 +33,7 @@ const Login = () => {
                     style={Styles.LoginTextInput}
                     onChangeText={ChangeEmail}
                     value={Email}
-                    // placeholder='SammySlug@ucsc.edu'
+                // placeholder='SammySlug@ucsc.edu'
                 />
                 <Text style={Styles.TextInputTitle}>Password</Text>
                 <TextInput
@@ -42,7 +45,7 @@ const Login = () => {
                 <View style={Styles.LoginActionRow}>
                     <TouchableOpacity disabled={loginDisabled || signupDisabled} onPress={async () => {
                         setLoginDisabled(true);
-                        await UserSignIn({Email, Password});
+                        await UserSignIn({ Email, Password });
                         setLoginDisabled(false);
                     }}>
                         <View style={Styles.LoginActionButton}>
@@ -51,7 +54,7 @@ const Login = () => {
                     </TouchableOpacity>
                     <TouchableOpacity disabled={loginDisabled || signupDisabled} onPress={async () => {
                         setSignupDisabled(true);
-                        await UserSignUp({Email, Password});
+                        await UserSignUp({ Email, Password });
                         setSignupDisabled(false);
                     }}>
                         <View style={Styles.LoginActionButton}>
@@ -59,6 +62,10 @@ const Login = () => {
                         </View>
                     </TouchableOpacity>
                 </View>
+            </View>
+
+            <View style={Styles.LoginHeader}>
+                <Text style={Styles.LoginHeaderText}>SlugBro is an April Fool's app created by Jacob Dennon and Daniel Chang. It's a simple app where you can "bro" anyone else who has the app </Text>
             </View>
         </View>
     );
